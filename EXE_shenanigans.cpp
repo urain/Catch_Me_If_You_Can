bc95fe10e11ae58db17e61a5773e4ac50ad47d14
urain <urain@users.noreply.github.com>
2019-12-17 23:51:28 -0500

    Create EXE_shenanigans.cpp

---
+++ EXE_shenanigans.cpp
@@ -0,0 +1,44 @@
+#include "stdafx.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <Windows.h>
+
+extern "C" int MyAsmFunction(void*);
+
+#pragma optimize( "", off) // disables optimizations which cause this to be removed since it has no refs. 
+						   // it is called by doing math in the asm function
+void CatchThisThread() {
+	std::cout << "Hey This Is The String You Need To Catch...\n" << std::endl;
+	return;
+}
+#pragma optimize( "", on)
+
+int main()
+{
+	//HANDLE myThread = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)CatchThisThread, 0, 0, 0);
+	//WaitForSingleObject(myThread, -1);
+	std::cout << "Hey You Didn't Catch The String\n" << std::endl;
+	while (TRUE) {
+		continue;
+	}
+    return 0;
+}
+
+
+void NTAPI tls_callback1(PVOID DLLHandle, DWORD dwReason, PVOID) {
+	// modify code if debugger is present
+	DWORD myProts = 0;
+	int xxx = VirtualProtect((void*)&main, 1024, 0x40, &myProts);
+	MyAsmFunction((void*)&main);// , (void*)&CatchThisThread); // rcx, rdx
+	xxx = VirtualProtect((void*)&main, 1024, myProts, &myProts);
+	return;
+}
+
+#pragma comment (linker, "/INCLUDE:_tls_used")
+#pragma comment (linker, "/INCLUDE:p_tls_callback1")
+#pragma const_seg(push)
+#pragma const_seg(".CRT$XLAAA")
+EXTERN_C const PIMAGE_TLS_CALLBACK p_tls_callback1 = tls_callback1;
+#pragma const_seg(pop)
